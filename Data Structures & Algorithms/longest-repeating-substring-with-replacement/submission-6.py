822498f4f2ccc47768c122aef189cdb0fa0fdc5b
Jingwen Xu <selinaxu.985@gmail.com>
2026-08-07 14:34:14 -0400

    Add: longest-repeating-substring-with-replacement - submission-6

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-6.py
@@ -0,0 +1,23 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        longest = 0
+        left = 0
+        window = defaultdict(int)
+        # window = {}
+        max_ch_len = 0
+
+        for right in range(len(s)):
+            # window[s[right]]=window.get(s[right],0)+1
+            window[s[right]]+=1
+            
+            max_ch_len = max(max_ch_len, window[s[right]]) # instead of using max(window.values()) to iterate over the whole map, comparing two numbers is much faster, improve from 80-84ms to 48-52 ms
+            # max_ch_len = max(window.values())
+
+            # The rest_total_length is alwyas linked to the siz eof the window
+            while(right-left+1) - max_ch_len > k:
+                window[s[left]] -= 1
+                left+=1
+            
+            longest = max(longest, right-left+1)
+
+        return longest
