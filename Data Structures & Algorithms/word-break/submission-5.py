dacdef1230c6980c490f1c30ed31b59bf3db2e9c
Jingwen Xu <selinaxu.985@gmail.com>
2026-08-20 14:18:18 -0400

    Add: word-break - submission-5

---
+++ Data Structures & Algorithms/word-break/submission-5.py
@@ -0,0 +1,42 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        # n = len(s)
+        # # turn wordDict into set for O(1) lookup
+        # wordSet = set(wordDict)
+        # # get the maximum length of the wordDict
+        # t = 0
+        # for w in wordDict:
+        #     t = max(t, len(w))
+        # # use hashmap to remeber the same substring result
+        # memo = {n : True}
+
+        # def match(i):
+        #     if i in memo:
+        #         return memo[i]
+        #     if i == len(s):
+        #         return True
+            
+        #     for j in range(i, min(n, i+t)):
+        #         # for new ending s[i:j], we do not need to check j one position by one position
+        #         if s[i:j+1] in wordSet:
+        #             if match(j + 1):
+        #                 memo[i] = True
+        #                 return True
+        #     memo[i] = False
+        #     return False
+        
+        # return match(0)
+        
+        # method 2: Bottom-up DP
+        # dp[i] means whther the substring s[i:] can be segmented
+        dp = [False] * (len(s) +1)
+        dp[len(s)] = True
+
+        for i in range(len(s) -1, -1, -1):
+            for w in wordDict:
+                if (i + len(w) <= len(s) and s[i:i+len(w)] == w):
+                    dp[i] = dp[i+len(w)]
+                if dp[i]:
+                    break # break the inner for loop
+        return dp[0]
+        
