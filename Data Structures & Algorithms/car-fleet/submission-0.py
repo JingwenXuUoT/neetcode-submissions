d413c3cf2cdb3191e4d5ef9904b720bbb34d7d09
Jingwen Xu <selinaxu.985@gmail.com>
2026-08-31 14:20:16 -0400

    Add: car-fleet - submission-0

---
+++ Data Structures & Algorithms/car-fleet/submission-0.py
@@ -0,0 +1,19 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        n = len(position)
+        car_fleer = n # n is when no car catch up with the car ahead, and each car reaches the destination individually.
+        # can catch up before or at destination:
+        # (target-front_position) / front_speed >= (target-back_position) / back_speed, if true, car_fleet -= 1
+        position_speed = [(position[i], speed[i]) for i in range(n)]
+        position_speed.sort(key=lambda x: x[0], reverse=True) # sort in descending order ensures clarity about the final speed of each car
+        # iterate over this position_fleet array, maintain a stack for the time of each car reach the target, check if current car's time is less than or equal to the top of the stack, it joins the same fleet, otherwise it forms a new fleet then push its time onto the stack(since the back car cannot pass the car in front of it, so only need to check the one top element in the stack)
+        # the length of the stack at the end represents the total number of fleets formed
+        time_stack = []
+        for ps in position_speed:
+            cur_time = (target - ps[0]) / ps[1]
+            if len(time_stack) == 0 or cur_time > time_stack[-1]:
+                time_stack.append(cur_time)
+        
+        return len(time_stack)
+
+
