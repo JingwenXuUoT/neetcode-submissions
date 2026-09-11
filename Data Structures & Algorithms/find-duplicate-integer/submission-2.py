37d65f728b5fe1c0a308105f312452d23e923636
Jingwen Xu <selinaxu.985@gmail.com>
2026-09-11 14:23:53 -0400

    Add: find-duplicate-integer - submission-2

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-2.py
@@ -0,0 +1,56 @@
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        # n = max(nums)
+        # n_set = set()
+        # for num in nums:
+        #     if num in n_set:
+        #         return num
+        #     n_set.add(num)
+        # return n
+        # O(n), O(n)
+
+        # Method 2: Binary Search
+        # use binary search on the value range
+        # if all numbers from 1 to mid appeared at most once,
+        # then the count of numbers <= mid should be <= mid, 
+        # otherwise, meaning the duplicate must be in the range [1,mid]
+        # continuous shrink the candidate range, when low== high, the value is the result
+        # TC: O(nlogn), SC: O(1)
+        n = len(nums)
+        low = 1
+        high = n - 1
+        while low < high:
+            mid = low + (high - low) // 2
+            lessOrEqual = sum( 1 for num in nums if num <= mid)
+            if lessOrEqual <= mid:
+                low = mid + 1
+            else:
+                high = mid
+
+        return low
+        # method 3: optimize SC to O(1)
+        # treating the array as linkedlist
+        # from an index i, the next index is nums[i]
+        # every value is in the range 1 to len(nums)-1, so every value is also a valid index
+        # since one value is repeated, multiple indices point to the same next index.
+        # Following the indices starting from index 0 nust therefore lead to a cycle, whose entry corresponds to the duplicate value
+        # We can use Floyd's cycle detection algorithm: fast, slow pointer
+        # the point where slow and fast meet again is the entry point of the cycle, also the duplicate value
+        # TC: O(n), SC: O(1)
+        slow = 0
+        fast = 0
+        # first find the cycle
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            if slow == fast:
+                break
+        
+        slow2 = 0
+        # then find the netry point
+        while True:
+            slow = nums[slow]
+            slow2 = nums[nums[slow2]]
+            if slow == slow2:
+                return slow
+        
